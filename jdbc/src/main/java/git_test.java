65e6b8b535f8208949e2db27868ea5a793beddcd
lk <lk@gmnm.com>
2021-06-08 11:07:55 +0800

    first commit

---
+++ jdbc/src/main/java/git_test.java
@@ -0,0 +1,10 @@
+/**
+ * @author internationale
+ * @subject
+ * @create 2021-06-08-11:06
+ */
+public class git_test {
+    public static void main(String[] args) {
+        System.out.println("hello git");
+    }
+}
